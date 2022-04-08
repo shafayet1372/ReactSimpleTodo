b832cc8aed7c012b871d97a58b6501d3457dc1b8
shafayet <shafayethossain777@gmail.com>
2022-04-09 00:37:26 +0600

    file modified

--- src/component/TitleImage.jsx
+++ src/component/TitleImage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import style from "../style.module.css";
 import { Row, Col } from "reactstrap";
+import image from "../image/todo.png";
 export default function Title() {
   return (
     <Row className="text-center">
       <Col>
-        <img src="image/logo192.png" alt="sadsadsad" />
+        <img src={image} alt="sadsadsad" className={`${style.img} img-fluid`} />
       </Col>
     </Row>
   );
